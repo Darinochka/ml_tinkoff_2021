b47acae60b017160a2c1ab543e6c5c96feb753fb
Darinka <39233990+Darinochka@users.noreply.github.com>
2022-01-08 05:26:48 +0500

    userBasedRecommendation

---
+++ notebooks/lesson_10_recommend/hw/utils.py
@@ -0,0 +1,65 @@
+import numpy as np
+from typing import Optional, Union, Tuple
+
+
+def euclidean_distance(x: np.array, y: np.array) -> float:
+    """
+    Calculate euclidean distance between points x and y
+    Args:
+        x, y: two points in Euclidean n-space
+    Returns:
+        Length of the line segment connecting given points
+    """
+    assert x.shape == y.shape
+
+    return np.sqrt(np.sum((x - y) ** 2))
+
+
+def euclidean_similarity(x: np.array, y: np.array) -> float:
+    """
+    Calculate euclidean similarity between points x and y
+    Args:
+        x, y: two points in Euclidean n-space
+    Returns:
+        Similarity between points x and y
+    """
+    return 1 / (1 + euclidean_distance(x, y))
+
+
+def pearson_similarity(x: np.array, y: np.array) -> float:
+    """
+    Calculate a Pearson correlation coefficient given 1-D data arrays x and y
+    Args:
+        x, y: two points in n-space
+    Returns:
+        Pearson correlation between x and y
+    """
+    f = lambda z: np.sum((z - z.mean()) ** 2)
+
+    return np.sum((x - x.mean()) * (y - y.mean())) / np.sqrt(f(x) * f(y))
+
+
+def apk(actual: np.array, predicted: np.array, k: int = 10) -> float:
+    """
+    Compute the average precision at k
+    Args:
+        actual: a list of elements that are to be predicted (order doesn't matter)
+        predicted: a list of predicted elements (order does matter)
+        k: the maximum number of predicted elements
+    Returns:
+        The average precision at k over the input lists
+    """
+    pass
+
+
+def mapk(actual: np.array, predicted: np.array, k: int = 10) -> float:
+    """
+    Compute the mean average precision at k
+    Args:
+        actual: a list of lists of elements that are to be predicted
+        predicted: a list of lists of predicted elements
+        k: the maximum number of predicted elements
+    Returns:
+        The mean average precision at k over the input lists
+    """
+    pass
