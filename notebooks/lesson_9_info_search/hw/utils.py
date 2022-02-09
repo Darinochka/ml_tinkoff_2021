237beecf2731c334a928753ac9086be130158906
Darinka <39233990+Darinochka@users.noreply.github.com>
2022-02-09 04:05:17 +0300

    finish:

--- notebooks/lesson_9_info_search/hw/utils.py
+++ notebooks/lesson_9_info_search/hw/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 from typing import Optional, Union, Tuple
-from functools import reduce
+from functools import lru_cache, reduce
 import math
 
 def apk(actual: np.array, predicted: np.array, k: int = 10) -> float:
@@ -21,7 +21,7 @@ def apk(actual: np.array, predicted: np.array, k: int = 10) -> float:
 
 def mapk(actual: np.array, predicted: np.array, k: int = 10) -> float:
     """
-    Compute the mean average precision at k (map@K)
+    Compute the mean average precision at k
     Args:
         actual: a list of lists of elements that are to be predicted
         predicted: a list of lists of predicted elements
@@ -29,14 +29,14 @@ def mapk(actual: np.array, predicted: np.array, k: int = 10) -> float:
     Returns:
         The mean average precision at k over the input lists
     """
-    return sum(lambda x, y: apk(x, y, k), zip(actual, predicted)) / len(actual)
+    len_u = len(actual)
+    return reduce(lambda a, x: a + x, map(lambda x: apk(actual[x], predicted[x], k), range(len_u))) / len_u
 
-def dcgk(scores: np.array, predicted: np.array, k: int = 10) -> float:
+def dcgk(scores: np.array, k: int = 10) -> float:
     """
     Compute the discounted cumulative gain at K (DCG@K)
     Args:
-        actual: a list of elements that are to be predicted
-        predicted: a list of predicted elements
+        scores: a list of scores of the elements that are to be predicted
         k: the maximum number of predicted elements
     Returns:
         The the discounted cumulative gain at K over the input lists
@@ -65,7 +65,27 @@ def mrr(actual: np.array, predicted: np.array, k: int = 10) -> float:
         result += rrk(act, pred, k)
     return result / len(actual)
 
+def prel(scores: np.array, i: int):
+    if scores[i] > 0:
+        return scores[i]
+    return 0
 
+def plook(scores: np.array, i: int) -> float:
+    if i == 0:
+        return 1
+    else:
+        return plook(scores, i-1) * (1 - prel(scores, i-1))
     
+def pfound(scores: np.array, k: int = 10) -> float:
+    """
+    Compute the PFound at K (PFound@K)
+    Args:
+        scores: a list of scores of the elements that are to be predicted
+        k: the maximum number of predicted elements
+    Returns:
+        The the discounted cumulative gain at K over the input lists
+    """
+    return sum(map(lambda i: plook(scores, i) * prel(scores, i), range(k)))
+
 
 
